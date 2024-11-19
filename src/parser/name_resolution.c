89bda7eb792c82926533e1064c6de38e2ca0382c
kangmin <kangmin5505@gmail.com>
2024-11-19 16:42:43 +0900

    check meta_class and data_type

--- src/parser/name_resolution.c
+++ src/parser/name_resolution.c
@@ -3084,22 +3084,25 @@ pt_bind_names (PARSER_CONTEXT * parser, PT_NODE * node, void *arg, int *continue
       save = node->info.insert.odku_assignments;
       node->info.insert.odku_assignments = NULL;
 
-      /* TODO: refactor to a function */
-      node1 = node->info.insert.value_clauses->info.node_list.list;
-      while (node1)
-        {
-          if (node1->node_type == PT_NAME)
-            {
-              /* TODO: add a message */
-	      PT_ERRORc (parser, node1, er_msg ());
-              goto insert_end;
-            }
-          node1 = node1->next;
-        }
-
       if (node->info.insert.spec->info.spec.remote_server_name == NULL)
 	{
 	  parser_walk_leaves (parser, node, pt_bind_names, bind_arg, pt_bind_names_post, bind_arg);
+	}
+
+      /* TODO: refactor to a function */
+      node1 = node->info.insert.value_clauses->info.node_list.list;
+      while (node1)
+	{
+	  if (node1->node_type == PT_NAME && node1->info.name.meta_class == PT_NORMAL && !node1->data_type)
+	    {
+	      /* TODO: add a message */
+	      //       er_set (ER_WARNING_SEVERITY, ARG_FILE_LINE, ER_OBJ_INVALID_ATTRIBUTE, 1, name);
+	      //       PT_ERRORc (parser, node1, er_msg ());
+	      //       PT_ERRORmf (parser, node, MSGCAT_SET_PARSER_SEMANTIC, MSGCAT_SEMANTIC_METH_DOESNT_EXIST,
+	      //                node->info.method_call.method_name->info.name.original);
+	      goto insert_end;
+	    }
+	  node1 = node1->next;
 	}
 
       /* Check for double assignments */
